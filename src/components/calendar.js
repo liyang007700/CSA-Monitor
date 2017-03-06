1f452b26b115f3c974e811bbda09d88ff6cf05f7
liyang007700 <liyang007700@163.com>
2017-03-06 11:29:23 +0800

    calendar bug fixed

--- src/components/calendar.js
+++ src/components/calendar.js
@@ -1,67 +1,87 @@
 /*jshint esversion: 6 */
 import template from '../templates/calendar.html';
-import '../components/calendar/calendarWeekTable';
+//import '../components/calendar/calendarWeekTable';
 import '../components/calendar/calendarWeekEvent';
 
 const componentCalendar = {
 	template: template,
 	data: function() {
 		return {
-			date: [
-				"2017-02-27T00:00", "2017-02-28", "2017-03-01", "2017-03-02",
-				"2017-03-03",
-				"2017-03-04", "2017-03-05"
-			],
-			events: [{
-				"text": "CSA RoadShow HK",
-				"type": "demo",
-				"startTime": "2017-02-27T03:00",
-				"endTime": "2017-02-27T04:00"
+			agenda: [{
+				events: [{
+					"date": "2017-02-27",
+					"text": "CSA RoadShow HK",
+					"type": "demo",
+					"startTime": "2017-02-27T03:00",
+					"endTime": "2017-02-27T04:00"
+				}]
 			}, {
-				"text": "Page Manager quick fix",
-				"type": "release",
-				"startTime": "2017-02-28T05:00",
-				"endTime": "2017-02-28T06:00"
+				events: [{
+					"date": "2017-02-28",
+					"text": "Page Manager quick fix",
+					"type": "release",
+					"startTime": "2017-02-28T05:00",
+					"endTime": "2017-02-28T06:00"
+				}]
 			}, {
-				"text": "CSA RoadShow Beijing",
-				"type": "demo",
-				"startTime": "2017-03-01T04:00",
-				"endTime": "2017-03-01T05:00"
+				events: [{
+					"date": "2017-03-01",
+					"text": "CSA RoadShow Beijing",
+					"type": "demo",
+					"startTime": "2017-03-01T04:00",
+					"endTime": "2017-03-01T05:00"
+				}, {
+					"date": "2017-03-01",
+					"text": "SSO outage",
+					"type": "unplanned",
+					"startTime": "2017-03-01T06:00",
+					"endTime": "2017-03-01T07:00"
+				}]
 			}, {
-				"text": "SSO outage",
-				"type": "unplanned",
-				"startTime": "2017-03-01T06:00",
-				"endTime": "2017-03-01T07:00"
+				events: [{
+					"date": "2017-03-02",
+					"text": "investor relationship",
+					"type": "demo",
+					"startTime": "2017-03-02T03:00",
+					"endTime": "2017-03-02T04:00"
+				}]
 			}, {
-				"text": "investor relationship",
-				"type": "demo",
-				"startTime": "2017-03-02T03:00",
-				"endTime": "2017-03-02T04:00"
+				events: [{
+					"date": "2017-03-03",
+					"text": "Framework Release",
+					"type": "planned",
+					"startTime": "2017-03-03T03:00",
+					"endTime": "2017-03-03T04:00"
+				}, {
+					"date": "2017-03-03",
+					"text": "Learning Hub Release",
+					"type": "release",
+					"startTime": "2017-03-03T04:00",
+					"endTime": "2017-03-03T05:00"
+				}, {
+					"date": "2017-03-03",
+					"text": "OneScore Release",
+					"type": "release",
+					"startTime": "2017-03-03T05:00",
+					"endTime": "2017-03-03T06:00"
+				}]
 			}, {
-				"text": "Framework Release",
-				"type": "planned",
-				"startTime": "2017-03-03T03:00",
-				"endTime": "2017-03-03T04:00"
+				events: [{
+					"date": "2017-03-04",
+					"text": "",
+					"type": "",
+					"startTime": "2017-03-04T00:00",
+					"endTime": "2017-03-04T00:00"
+				}]
 			}, {
-				"text": "Learning Hub Release",
-				"type": "release",
-				"startTime": "2017-03-03T04:00",
-				"endTime": "2017-03-03T05:00"
-			}, {
-				"text": "OneScore Release",
-				"type": "release",
-				"startTime": "2017-03-03T05:00",
-				"endTime": "2017-03-03T06:00"
-			}, {
-				"text": "DB2 Data backup",
-				"type": "other",
-				"startTime": "2017-03-05T02:00",
-				"endTime": "2017-03-05T05:00"
-			}],
-			hours: ["12AM", "1AM", "2AM", "3AM", "4AM", "5AM", "6AM", "7AM", "8AM",
-				"9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM", "6PM",
-				"7PM", "8PM", "9PM", "10PM", "11PM"
-			]
+				events: [{
+					"date": "2017-03-05",
+					"text": "DB2 Data backup",
+					"type": "other",
+					"startTime": "2017-03-05T02:00",
+					"endTime": "2017-03-05T05:00"
+				}]
+			}]
 		};
 	},
 	methods: {},
--- src/components/calendar/calendarWeekEvent.js
+++ src/components/calendar/calendarWeekEvent.js
@@ -2,35 +2,27 @@
 import template from '../../templates/calendar/calendarWeekEvent.html';
 
 Vue.component('event-block', {
-    props: ['item', 'date'],
+    props: ['item'],
     template: template,
     data: function() {
         return {
             styleObject: {
                 top: '0',
-                left: '0',
                 height: '80px'
             }
         };
     },
     methods: {
         computeStyle: function() {
-            let widthContainer = document.getElementById(
-                "containerTable");
-            console.log(widthContainer);
-            let totalMinute = 10080;
-            let zeroTime = Date.parse(this.date[0]);
+            let zeroTime = Date.parse(this.item.date);
             let startTime = (Date.parse(this.item.startTime) -
                 zeroTime) / 60000;
             let duration = ((Date.parse(this.item.endTime) -
-                zeroTime) / 60000) - startTime;
+                Date.parse(this.item.startTime)) / 60000);
             console.log('startTime: ' + startTime);
             console.log('duration: ' + duration);
-            this.styleObject.left = (Math.round(startTime / 1440) *
-                    176 +
-                    53.21) +
-                'px';
-            this.styleObject.top = ((startTime % 1440) / 60) * 80 +
+
+            this.styleObject.top = (startTime / 60) * 80 +
                 'px';
             this.styleObject.height = (duration / 60) * 80 + 'px';
         }
