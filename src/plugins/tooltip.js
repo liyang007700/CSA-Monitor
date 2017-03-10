0bc83150fd605cf2058b396466bfc69cf9788993
liyang007700 <liyang007700@163.com>
2017-03-10 15:51:10 +0800

    Network analysis version 0.1

--- src/plugins/tooltip.js
+++ src/plugins/tooltip.js
@@ -8,55 +8,56 @@ const POSITION_FN = {
 	bottom: positionTooltipToBottom
 };
 
-Vue.directive('tooltip', function(el, binding){
+Vue.directive('tooltip', function(el, binding) {
 	const $body = document.querySelector('body');
-    let $tooltip = null;
-	
-    el.addEventListener('mouseenter', (e) => {
-    	$tooltip = createTooltip($body, el, binding);
-    	$tooltip.style.opacity = 1;
+	let $tooltip = null;
+
+	el.addEventListener('mouseenter', (e) => {
+		$tooltip = createTooltip($body, el, binding);
+		$tooltip.style.opacity = 1;
 	});
-    
-    el.addEventListener('mouseleave', (e) => {
-    	$tooltip.style.display = 'none';
-    	$tooltip.style.opacity = 0;
+
+	el.addEventListener('mouseleave', (e) => {
+		$tooltip.style.display = 'none';
+		$tooltip.style.opacity = 0;
 	});
 });
 
-function createTooltip($body, $el, binding){
+function createTooltip($body, $el, binding) {
 	const title = binding.value.title || '';
 	const position = binding.value.position || TOOLTIP_DEFAULT_POSITION;
-	
-	const $tooltip = document.querySelector('.m-tooltip') || document.createElement('div');
+
+	const $tooltip = document.querySelector('.m-tooltip') || document.createElement(
+		'div');
 	$tooltip.className = 'm-tooltip';
-	
+
 	$tooltip.innerHTML = '<span>' + title + '</span>';
 	$tooltip.style.backgroundColor = TOOLTIP_DEFAULT_BACKGROUND_COLOR;
 	$tooltip.style.display = 'initial';
 	$body.appendChild($tooltip);
-	
+
 	POSITION_FN[position]($el, $tooltip, binding);
-	
+
 	return $tooltip;
 }
 
-function positionTooltipToTop($el, $tooltip, binding){
+function positionTooltipToTop($el, $tooltip, binding) {
 	const $elPosition = $el.getBoundingClientRect();
-	
+
 	$tooltip.className = 'm-tooltip top';
 	$tooltip.style.height = 'initial';
 	$tooltip.style.lineHeight = 'initial';
 	$tooltip.style.top = $elPosition.top - $tooltip.offsetHeight - 12 + 'px';
-	$tooltip.style.left = $elPosition.left + 'px';
+	$tooltip.style.left = $elPosition.left - 10 + 'px';
 }
 
-function positionTooltipToLeft($el, $tooltip, binding){
-	
+function positionTooltipToLeft($el, $tooltip, binding) {
+
 }
 
-function positionTooltipToRight($el, $tooltip, binding){
+function positionTooltipToRight($el, $tooltip, binding) {
 	const $elPosition = $el.getBoundingClientRect();
-	
+
 	$tooltip.className = 'm-tooltip right';
 	$tooltip.style.height = $elPosition.height + 'px';
 	$tooltip.style.lineHeight = $elPosition.height - 14 + 'px';
@@ -64,6 +65,6 @@ function positionTooltipToRight($el, $tooltip, binding){
 	$tooltip.style.left = $elPosition.left + 12 + $el.offsetWidth + 'px';
 }
 
-function positionTooltipToBottom($el, $tooltip, binding){
-	
+function positionTooltipToBottom($el, $tooltip, binding) {
+
 }
